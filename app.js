0972b261e05d0757b72b6c24da9fdfc7562c4fb7
Victor Barbosa <vicldb99@gmail.com>
2020-01-08 20:50:31 -0300

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+class sectionOne extends React.Component {
+    render() {
+        return (
+            <div className="container">Hello World!</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <sectionOne />,
+    document.getElementById('root')
+  );
